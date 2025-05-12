d7b94fbd62eb950913de9ab1f337f80b7f54676c
lunahcujar <lherrera95@ucatolica.edu.co>
2025-05-12 15:49:15 -0500

    coreccion dbconnection

--- dbconnection.py
+++ dbconnection.py
@@ -1,21 +1,9 @@
 from contextlib import asynccontextmanager
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-import os
 
-# Cargar variables del archivo .env
-load_dotenv()
-
-# Obtener variables desde el entorno
-DB_HOST = os.getenv("CLEVER_HOST")
-DB_NAME = os.getenv("CLEVER_DATABASE")
-DB_USER = os.getenv("CLEVER_USER")
-DB_PASSWORD = os.getenv("CLEVER_PASSWORD")
-DB_PORT = os.getenv("CLEVER_PORT")
-
-# Formar la URL de conexión para PostgreSQL asíncrono
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# URL de conexión completa proporcionada por Clever
+DATABASE_URL = "postgresql://udafrfxeywqopsnngsxy:qOpKiLpt06qQF3VFmbiSllPf7J7ZW6@byjnneiuugcgy4m2iqlh-postgresql.services.clever-cloud.com:50013/byjnneiuugcgy4m2iqlh"
 
 # Crear el motor de conexión asíncrono
 engine = create_async_engine(DATABASE_URL, echo=True)
@@ -33,6 +21,6 @@ async def get_db_session():
     async with AsyncSessionLocal() as session:
         yield session
 
-# Motor síncrono si es necesario
+# Motor síncrono si es necesario (por si necesitas usarlo en algunas partes de tu código)
 def get_engine():
     return engine
